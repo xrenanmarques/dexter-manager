30a6738f0689cfef73c6334f709323b1a885f64e
Renan Marques <renan.maarques2@gmail.com>
2019-07-31 17:50:01 -0300

    componente logo

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,7 +1,10 @@
 import './Logo.css'
+import logo from '../../assets/imgs/logo1.png'
 import React from 'react'
 
-export default props => 
+export default props =>
     <aside className="logo">
-        Logo
+        <a href="/" className="logo">
+            <img src={logo} alt="logo" />
+        </a>
     </aside>
