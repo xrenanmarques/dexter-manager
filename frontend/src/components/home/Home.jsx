dedcae0a4721be38705711f3652ea08f1d9e58fc
Renan Marques <renan.maarques2@gmail.com>
2019-07-31 19:58:40 -0300

    componente cadastro usuario

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Dexter Manager">
+        <div className="display-4">Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Gerenciador de Usuários</p>
+    </Main>
